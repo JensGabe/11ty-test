2fb5407c53ff7a42c2fbb14a8bbb386e27e73239
Jens Gabe <github.info@planetiq.dk>
2025-09-21 17:21:08 +0200

    Try FTP deploy from Node

---
+++ .cpanel-ci.js
@@ -0,0 +1,20 @@
+// .cpanel-ci.js
+
+import { deploy, excludeDefaults } from "@samkirkland/ftp-deploy";
+
+async function deployToCPanel() {
+  console.log("🚚 Deploy started");
+  await deploy({
+    server: process.env.FTP_SERVER,
+    username: process.env.FTP_USERNAME,
+    password: process.env.FTP_PASSWORD,
+    protocol: "ftps",
+    "log-level": "verbose",
+    "local-dir": "_site/",
+    "server-dir": "public_html/",
+    exclude: [...excludeDefaults, ".env", ".git/**", "node_modules/**", "vendor/**"],
+  });
+  console.log("🚀 Deploy done!");
+}
+
+deployToCPanel();
